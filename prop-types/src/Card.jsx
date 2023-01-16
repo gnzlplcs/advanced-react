f491233385cc4b880d6988ba6bbb8d0bbd363f74
gnzlplcs <palaciosgm@outlook.com>
2023-01-16 15:30:03 -0500

    :heavy_plus_sign: add prop-types dependency

--- prop-types/src/Card.jsx
+++ prop-types/src/Card.jsx
@@ -1,21 +1,20 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
 
 function Card(props) {
-    const styles = {
-        backgroundColor: props.cardColor,
-        height: props.height,
-        width: props.width
-    }
+  const styles = {
+    backgroundColor: props.cardColor,
+    height: props.height,
+    width: props.width,
+  };
 
-    return (
-        <div style={styles}></div>
-    )
+  return <div style={styles}></div>;
 }
 
 Card.defaultProps = {
-    cardColor: "blue",
-    height: 100,
-    width: 100
-}
+  cardColor: "blue",
+  height: 100,
+  width: 100,
+};
 
-export default Card
+export default Card;
