33a9dda87f5647d95f47fb71022700c59dce596d
gnzlplcs <palaciosgm@outlook.com>
2023-01-16 16:08:44 -0500

    :construction: practice template

---
+++ defaultProps-propTypes-practice/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import RoundedImg from "./RoundedImg"
+
+function App() {    
+    return (
+        <div>
+            <RoundedImg src="https://picsum.photos/id/237/300/300" borderRadius="10px" />
+        </div>
+    )
+}
+
+export default App
