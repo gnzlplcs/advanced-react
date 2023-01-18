2720db8242f08ea3aa94bb09d926025457af37e4
gnzlplcs <palaciosgm@outlook.com>
2023-01-17 19:20:25 -0500

    :construction: react children completed

--- 04-react-children/src/App.jsx
+++ 04-react-children/src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react"
+import CTA from "./CTA"
 
 function App() {
     return (
         <div>
-            Code here
+            <CTA position="right">
+                <h1>This is an important CTA</h1>
+                <button>Click me now or you'll miss out!</button>
+            </CTA>
+
+            <CTA>
+                <form>
+                    <input type="email" placeholder="Enter email address here"/>
+                    <br />
+                    <br />
+                    <button>Submit</button>
+                </form>
+            </CTA>
         </div>
     )
 }
---
+++ 04-react-children/src/CTA.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function CTA(props) {
+    return (
+        <div className="border">
+            {props.children}
+        </div>
+    )
+}
+
+export default CTA
