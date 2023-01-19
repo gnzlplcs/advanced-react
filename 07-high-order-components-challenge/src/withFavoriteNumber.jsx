fc459376701f0eae4dffd8122ee191abf03e07c5
gnzlplcs <palaciosgm@outlook.com>
2023-01-19 13:27:18 -0500

    :boom: init template challenge

---
+++ 07-high-order-components-challenge/src/withFavoriteNumber.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+/**
+ * A function that takes a component as its first argument and returns a new component that wraps the given component, providing extra capabilities to it.
+ */
+export function withFavoriteNumber() {
+    return ???
+}
