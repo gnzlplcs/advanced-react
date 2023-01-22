01f6a713c4aba2aacafab78deae77e735d23e7aa
gnzlplcs <palaciosgm@outlook.com>
2023-01-22 07:56:02 -0500

    :construction: shoulComponentUpdate completed
    I do not understand very well yet

--- 14-shouldComponentUpdate/src/GrandParent.jsx
+++ 14-shouldComponentUpdate/src/GrandParent.jsx
@@ -1,17 +1,26 @@
-import React, {Component} from "react"
-import Parent from "./Parent"
+import React, { Component } from "react";
+import Parent from "./Parent";
 
 class GrandParent extends Component {
-    render() {
-        console.log("[👴🏼]   [ ]   [ ]   [ ] rendered")
-        return (
-            <div>
-                <p>I'm a GrandParent Component</p>
-                <Parent />
-                <Parent />
-            </div>
-        )
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log(nextProps) // this prompts an empty object
+    console.log(this.props.count)
+    if (nextProps.count === this.props.count) {
+      return false;
     }
+    return true;
+  }
+
+  render() {
+    console.log("[👴🏼]   [ ]   [ ]   [ ] rendered");
+    return (
+      <div>
+        <p>I'm a GrandParent Component</p>
+        <Parent />
+        <Parent />
+      </div>
+    );
+  }
 }
 
-export default GrandParent
+export default GrandParent;
