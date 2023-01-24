c63692e58b9a758b8b7d950317e66846a88710e2
gnzlplcs <palaciosgm@outlook.com>
2023-01-23 19:52:38 -0500

    :construction: contextType template

---
+++ 18-contextType/src/Button.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+
+class Button extends Component {
+    render() {
+        return (
+            <button className="light-theme">Switch Theme</button>
+        )
+    }
+}
+
+export default Button
