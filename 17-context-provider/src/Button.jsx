940f901b4967bb98784d10e07b45692de95ab04f
gnzlplcs <palaciosgm@outlook.com>
2023-01-23 19:50:09 -0500

    :construction: Context Provider lesson
    done

--- 17-context-provider/src/Button.jsx
+++ 17-context-provider/src/Button.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from "react"
-import ThemeContext from "./themeContext"
 
 class Button extends Component {
     render() {
--- 17-context-provider/src/main.jsx
+++ 17-context-provider/src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const ThemeContext = React.createContext();
+// ThemeContent.Provider & ThemeContecxt.Consumer
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeContext.Provider value={"light"}>
+      <App />
+    </ThemeContext.Provider>
+  </React.StrictMode>
+);
