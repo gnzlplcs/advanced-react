2de7ed1101e5e472c9d792ef7a38ff319d81b364
gnzlplcs <palaciosgm@outlook.com>
2023-01-25 08:13:30 -0500

    :boom: context challenge 2 template

---
+++ 21-context-challenge-2/src/Header.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+import UserContext from "./userContext"
+
+class Header extends Component {
+    static contextType = UserContext
+
+    render() {
+        const username = this.context
+        return (
+            <header>
+                <p>Welcome, {username}!</p>
+            </header>
+        )
+    }
+}
+
+export default Header
